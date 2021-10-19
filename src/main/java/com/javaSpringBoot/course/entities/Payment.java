a90ccf73d3e267cdeebb24c1b55a901b63a9030d
brunasilveira1993 <brunaasilveira@hotmail.com>
2021-10-18 22:09:34 -0300

    ManyToMany Association

--- src/main/java/com/javaSpringBoot/course/entities/Payment.java
+++ src/main/java/com/javaSpringBoot/course/entities/Payment.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.time.Instant;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -12,6 +15,8 @@ public class Payment implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Instant moment;
 
